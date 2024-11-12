84c12ef41294a85404f96be36e89b1f46fd4c4c8
negishi-o <abc.x.piko@gmail.com>
2024-11-12 18:55:13 +0900

    first commit

---
+++ src/test/java/com/udemy/ToDoTaskApp/ToDoTaskAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.ToDoTaskApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoTaskAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
